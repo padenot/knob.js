303ccefbac8d7bc7b77cd09333b2dc6294216397
Paul ADENOT <paul@paul.cx>
2011-09-20 18:51:31 +0200

    quadratic curve on mouseup

--- knob.js
+++ knob.js
@@ -40,35 +40,40 @@ function Knob(element, width, height) {
   var that = this;
 
   this.slider.addEventListener("mousedown", function(e) {
-    knob = that;
+    knob = {element: that,
+            x:e.clientX,
+            y:e.clientY,
+            dx: 0,
+            dy: 0};
   }, false);
 
   this.slider.addEventListener("click", function(e) {
     var m_y = e.clientY;
     var thumb = that.inside;
-    log(m_y);
     var value = ((m_y - that.slider.offsetTop) / that.slider.offsetHeight) * that.max;
     that.setValue(value);
   }, false);
 
   document.addEventListener("mousemove", function(e) {
-    if (knob) {
+    if (knob && !knob.decreasing) {
+      knob.dx = knob.x - e.clientX;
+      knob.dy = knob.y - e.clientY;
+      knob.x = e.clientX;
+      knob.y = e.clientY;
       var m_y = e.clientY;
-      log(m_y);
       if (m_y < that.slider.offsetTop) {
-        log(that.slider.offsetTop);
         m_y = that.slider.offsetTop;
       }
       if (m_y >= (that.slider.offsetHeight)) {
         m_y = that.slider.offsetHeight + that.slider.offsetTop;
       }
-      var value = (m_y - that.slider.offsetTop)/ that.slider.offsetHeight * knob.max;
-      knob.setValue(value);
+      var value = (m_y - that.slider.offsetTop)/ that.slider.offsetHeight * knob.element.max;
+      knob.element.setValue(value);
     }
   }, false);
 
   this.slider.addEventListener("DOMMouseScroll", function(e) {
-    var factor;
+    var factor = 1;
     if (e.altKey) {
       factor = 10;
     } else if (e.shiftKey) {
@@ -83,13 +88,32 @@ function Knob(element, width, height) {
 
   document.addEventListener("mouseup", function(e) {
     if (knob) {
-      knob = null;
+      if(Math.abs(knob.dy) > 0.1) {
+        document.querySelector('#speed').innerHTML = knob.dy;
+        var decr = 0.99;
+        function decrease() {
+          knob.decreasing = true;
+          log("decrease : " + knob.dy);
+          if (Math.abs(knob.dy) > 0.1 && Math.abs(knob.dy) > 0) {
+            knob.dy = knob.dy * decr;
+            decr = decr * decr;
+            var inc = knob.dy > 0 ? knob.element.increment * 0.1 : - knob.element.increment * 0.1;
+            knob.element.setValue(knob.element.value - inc);
+            setTimeout(decrease, 20);
+          } else {
+            log("setting to null");
+            knob = null;
+          }
+        }
+        decrease();
+      } else {
+        knob = null;
+      }
     }
   }, false);
 }
 
 Knob.prototype.setValue = function(value) {
-  log("about to set this.value to : " + value);
   if (value > this.max) {
     this.value = this.max;
   } else if (value < this.min) {
@@ -97,8 +121,6 @@ Knob.prototype.setValue = function(value) {
   } else {
     this.value = value;
   }
-
-  log("New value :" + this.value);
 
   this.root.querySelector('.inside').style.top = this.value / this.max * 100 + "%";
   this.label.value = ((this.value * 10) | 0) / 10;
